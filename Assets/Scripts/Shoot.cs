46b8096c49433105b9d1ba6e653a568cfa9e7069
Szymon Liszka <liszkaszymon@gmail.com>
2020-07-01 19:10:12 +0200

    Delay shot

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -16,6 +16,13 @@ public class Shoot : MonoBehaviour
         else
             GetComponent<Animator>().SetTrigger("RedAttack");
 
+        StartCoroutine(ExecuteAfterTime(bulletPrefab, 0.25f));
+    }
+
+    IEnumerator ExecuteAfterTime(GameObject bulletPrefab, float time)
+    {
+        yield return new WaitForSeconds(time);
+
         // Reset player position (necessary due to recoil)
         transform.position = new Vector3(-9.5f, -3.6f, 0.0f);
 
